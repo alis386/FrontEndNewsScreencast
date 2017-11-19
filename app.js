8bab6f37d8125ba08a919e723c073be7b718d1ff
Alicja Moczala <alis386@gmail.com>
2017-11-19 17:06:14 +0100

    form section and show/hide form

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.js-like').on('click', function(event){
+        event.preventDefault();
+        //.... zrob unlike
+        
+        $(this).text('Liked!')
+         .closest('.news-item')
+         .addClass('is-liked');
+    });
+    
+    
+    $('.js-add-link').on('click', function(event){
+         event.preventDefault();
+        
+        $('.js-form').toggleClass('is-visible');
+    });
+});
