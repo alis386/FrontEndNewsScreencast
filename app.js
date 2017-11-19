1bc5aa26614789d45392247e411cecc606a39431
Alicja Moczala <alis386@gmail.com>
2017-11-19 22:49:22 +0100

    opacity in modal-overlay is not working

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ $(document).ready(function(){
          .addClass('is-liked');
     });
     
-    
+    // add link
     $('.js-add-link').on('click', function(event){
          event.preventDefault();
         
         $('.js-form').toggleClass('is-visible');
     });
+    
+    // modal
+    $('.js-show-modal').on('click', function(event){
+        event.preventDefault();
+        
+        $('.js-modal').addClass('is-visible');
+        $('.js-modal-overlay').addClass('is-visible');
+    });
+    
+    $('.js-modal-overlay').on('click', function(event){
+        $('.js-modal').removeClass('is-visible');
+        $('.js-modal-overlay').removeClass('is-visible');
+    });
+    
 });
